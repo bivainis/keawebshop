f5c8d6860170270e8eea6a1ce9e86f14d93b16f0
Gediminas Bivainis <gediminas@bivainis.com>
2014-12-18 19:11:01 +0100

    generate xml file on product add

--- api/products/add_product.php
+++ api/products/add_product.php
@@ -71,13 +71,34 @@ function generateJSON($dbh){
 	$stmt = $dbh->prepare($query);
 	$stmt->execute();
 
-	$arr1 = array();
-	while ($arr = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		$toJSON[] = $arr;
+	$sXml = '<?xml version="1.0" encoding="UTF-8" ?>
+				<products>';
+
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$toJSON[] = $row;
+
+		$sId = $row['product_id'];
+		$sName = $row['product_name'];
+		$sPrice = $row['product_price'];
+		$sDesc = $row['product_description'];
+		$sPathToImage = $row['product_image'];
+		$sXml .= "<product>
+			<id>".$sId."</id>
+			<name>".$sName."</name>
+			<price>".$sPrice."</price>
+			<price>".$sDesc."</price>
+			<image>".$sPathToImage."</image>
+		</product>";
 	}
+
+	$sXml .= "</products>";
+
+	// save xml
+	file_put_contents("../../products.xml", $sXml);
 
 	$toReturn = array('products'=>$toJSON);
 
+	// save json
 	$fp = fopen('../../products.json', 'w+');
 	fwrite($fp, json_encode($toReturn));
 	fclose($fp);
